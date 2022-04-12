7d1af535cc7ba71373097eb37c2befd0c2881de5
andy201512 <1743535684@qq.com>
2022-04-12 23:51:52 +0800

    发布。

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   base: './',
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    chunkSizeWarningLimit: 800
   },
   resolve: {
     alias: {
