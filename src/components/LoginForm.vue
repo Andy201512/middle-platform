081b55cd7dc73210b10d625925aeefda0a84184d
andy201512 <1743535684@qq.com>
2022-04-15 14:25:00 +0800

    修复路由不带‘/middle-platform’导致访问出错问题。

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -24,7 +24,7 @@ export default defineComponent({
     const onFinish = (values: any) => {
       store.dispatch('saveUserAsync').then(() => {
         console.log('Success:', values);
-        router.push('/about')
+        router.push('/middle-platform/about')
       });
     };
 
